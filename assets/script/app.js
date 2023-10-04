91ebe4b98e5723064e6267f580300b78c22b4e15
Edna Medina <edna.medina3333@gmail.com>
2023-10-04 10:24:53 -0600

    Se agrega descripción del funcionamiento del programa

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,6 @@
+/*En este programa se consulta una API, se le pasa un nombre de usuario para que devuelva información del usuario que se le solicita y se muestra en pantalla su nombre, link de blog y ubicación (name, blog, location).
+*/
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('.name'); //se agrega . porque es una clase
@@ -13,21 +16,19 @@ async function displayUser(username) {
   //se agregó const data
   const data = await response.json(); 
   console.log(data);
-  $n.textContent = `${data.name}`; //tenía comillas simples cuando debe llevar backticks
+  //tenía comillas simples cuando debe llevar backticks
+  $n.textContent = `${data.name}`; 
   $b.textContent = `${data.blog}`;
   $l.textContent = `${data.location}`;
   } catch (err)  {
     handleError(err);
-    
-
   } 
-  
 }
 
 function handleError(err){
       console.log('OH NO!');
       console.log(err);
-      $n.textContent =`Algo salió mal: ${err}`; 
+      $n.textContent =`Algo salió mal: ${err}`; //se agregó signo $ a n
     }
 
 
